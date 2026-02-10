0a3fe870de492fb32fe97bc80050107c23f68827
MrEisbear <schmidt.patrick2010@gmail.com>
2026-02-10 23:00:56 +0000

    Stop tracking scripts directory

--- scripts/giftcard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -f "/home/SilkC/.venv/bin/activate" ]; then
-    source /home/SilkC/.venv/bin/activate
-fi
-PYTHON_BIN="/home/SilkC/.venv/bin/python"
-if [ ! -f "$PYTHON_BIN" ]; then
-    PYTHON_BIN="python3"
-fi
-$PYTHON_BIN /home/SilkC/scripts/issue_giftcard.py "$@"
